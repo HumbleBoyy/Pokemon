b3b2f614d1c4adeb25b134a2d3a5af8064e7dee5
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-12-06 20:53:07 +0500

    First commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
-let elName = document.querySelector(".name")
+let elPokemonList = document.querySelector(".list")
 
-let elPokemon = pokemons.map(item => {
-    console.log( item.name)
-})
 
-elName.textContent = elPokemon
+function renderCountries(arr, list){
+    
+    arr.map(item => {
+        let elCountryItem = document.createElement("li")
+        elCountryItem.className = "w-[250px] rounded-md overflow-hidden bg-slate-200 mt-4"
+
+        elCountryItem.innerHTML = `
+            <img class="w-full h-[200px] object-cover cursor-pointer" src=${item.img} width="100" height="160" alt=${item.name}/>
+            <div class="p-5 rounded-xl text-center cursor-pointer">
+              <p class="text-2xl font-bold text-gray-500 text-center">#${item.num}</p>
+              <h2 class="font-bold mb-2 text-[1.5rem] text-2xl">${item.name}</h2>
+              <button class="w-[200px] text-2xl ">${item.type}</button>
+            </div>
+        `
+        list.append(elCountryItem)
+    })
+}
+
+renderCountries(pokemons, elPokemonList)
