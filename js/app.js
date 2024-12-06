27c22a65cda0a0adbac06357bb7d87d99ea5ac5d
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-12-06 21:02:22 +0500

    New Commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 let elPokemonList = document.querySelector(".list")
-
+let elInput = document.querySelector(".input_text")
 
 function renderCountries(arr, list){
-    
+    list.innerHTML = null
     arr.map(item => {
         let elCountryItem = document.createElement("li")
         elCountryItem.className = "w-[250px] rounded-md overflow-hidden bg-slate-200 mt-4"
-
         elCountryItem.innerHTML = `
             <img class="w-full h-[200px] object-cover cursor-pointer" src=${item.img} width="100" height="160" alt=${item.name}/>
             <div class="p-5 rounded-xl text-center cursor-pointer">
@@ -20,3 +19,11 @@ function renderCountries(arr, list){
 }
 
 renderCountries(pokemons, elPokemonList)
+
+
+// Search input
+elInput.addEventListener("input", (event)=> {
+    const value = event.target.value.toLowerCase()
+    const searchedValue = pokemons.filter(item => item.name.toLowerCase().includes(value) || item.num.toLowerCase().includes(value))
+    renderCountries(searchedValue, elPokemonList)
+})
